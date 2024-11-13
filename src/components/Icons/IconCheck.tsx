56ab5d55caa5b7cedae6183ed1ad6742768bd53b
Gleb Bigma <glebbigma@gmail.com>
2024-11-13 21:02:48 +0200

    Clean code.

--- src/components/Icons/IconCheck.tsx
+++ src/components/Icons/IconCheck.tsx
@@ -7,8 +7,8 @@ const IconCheck = () => (
     xmlns='http://www.w3.org/2000/svg'
   >
     <path
-      fill-rule='evenodd'
-      clip-rule='evenodd'
+      fillRule='evenodd'
+      clipRule='evenodd'
       d='M15.746 6.66602L8.6031 14.666L7.87516 15.4813L7.12763 14.6839L4.27048 11.6363L5.72955 10.2684L7.83917 12.5187L14.2541 5.33398L15.746 6.66602Z'
       fill='#005CB2'
     />
