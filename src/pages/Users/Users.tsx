78e0c2c3b50eef3046ed4ad5a11b9e9a592d5233
Gleb Bigma <glebbigma@gmail.com>
2024-11-19 20:51:23 +0200

    Refactored UsersTable into a separate component.
    Took 21 minutes

--- src/pages/Users/Users.tsx
+++ src/pages/Users/Users.tsx
@@ -1,23 +1,11 @@
-import React, { useState, useMemo, ChangeEvent } from 'react';
-import { useGetUsersQuery, User } from '../../app/services/users';
-import {
-  useReactTable,
-  ColumnDef,
-  flexRender,
-  getCoreRowModel,
-  Row,
-} from '@tanstack/react-table';
-import moment from 'moment';
+import React, { useState, ChangeEvent } from 'react';
+import { useGetUsersQuery, } from '../../app/services/users';
+import { ColumnDef } from '@tanstack/react-table';
 import useDebounce from '../../hooks/useDebounce.ts';
 import SearchField from '../../components/UIComponents/SearchField/SearchField';
-import NotFound from '../../components/UIComponents/EmptyState/EmptyState';
 import UsersTableSettings from './UsersTableSettings';
 import Pagination from '../../components/UIComponents/Pagintaion/Pagination';
-import IconMale from '../../components/Icons/IconMale';
-import IconFemale from '../../components/Icons/IconFemale';
-import IconQuestion from '../../components/Icons/IconQuestion';
-import IconLoading from '../../components/Icons/IconLoading';
-import IconError from '../../components/Icons/IconError';
+import UsersTable from './UsersTable.tsx';
 
 export type CustomColumnDef<TData> = ColumnDef<TData> & {
   header: string;
@@ -56,163 +44,8 @@ const Users: React.FC = () => {
     }));
   };
 
-  const columns: CustomColumnDef<User>[] = useMemo(
-    () =>
-      [
-        {
-          header: 'Full Name',
-          cell: ({row}: { row: Row<User> }) => {
-            const {firstName, lastName, image} = row.original;
-
-            return (
-              <div className="flex flex-row items-center">
-                <img
-                  src={image}
-                  className="w-[32px] h-[32px] mr-[4px] border border-solid border-[rgba(0, 0, 0, 0.34)] rounded-full"
-                  alt=""
-                />
-                <span className="font-medium">
-                  {firstName} {lastName}
-                </span>
-              </div>
-            );
-          },
-        },
-        {
-          header: 'Birthday',
-          cell: ({row}: { row: Row<User> }) => {
-            const {birthDate, age} = row.original;
-
-            return (
-              <>
-                {moment(birthDate).format('DD.MM.YYYY')} ({age} years old)
-              </>
-            );
-          },
-        },
-        {
-          header: 'Gender',
-          cell: ({row}: { row: Row<User> }) => {
-            const {gender} = row.original;
-
-            return (
-              <>
-                {gender === 'male' ? (
-                  <div className="flex flex-row items-center">
-                    <IconMale /> Male
-                  </div>
-                ) : (
-                  <div className="flex flex-row items-center">
-                    <IconFemale /> Female
-                  </div>
-                )}
-              </>
-            );
-          },
-        },
-        {
-          header: 'Email',
-          accessorKey: 'email',
-        },
-        {
-          header: 'Phone',
-          accessorKey: 'phone',
-        },
-        {
-          header: 'Username',
-          accessorKey: 'username',
-        },
-        {
-          header: 'General info',
-          cell: ({row}: { row: Row<User> }) => {
-            const {bloodGroup, height, weight, hair} = row.original;
-
-            return (
-              <>
-                Bloodgroup: "{bloodGroup}"; Height: {Math.floor(height)};
-                Weight: {Math.floor(weight)}; Hair color: {hair.color}
-              </>
-            );
-          },
-        },
-        {
-          header: 'Domain',
-          accessorKey: 'userAgent',
-        },
-        {
-          header: 'IP',
-          accessorKey: 'ip',
-        },
-        {
-          header: 'Mac IP',
-          accessorKey: 'macAddress',
-        },
-      ].filter((column) => visibleColumns[column.header]),
-    [visibleColumns]
-  );
-
-  const table = useReactTable({
-    data: data?.users ?? [],
-    columns,
-    getCoreRowModel: getCoreRowModel(),
-  });
-
   const handleSearchChange = (event: ChangeEvent<HTMLInputElement>) => {
     setSearchTerm(event.target.value);
-  };
-
-  const renderUsersTableContent = () => {
-    if (isLoading) {
-      return <NotFound icon={<IconLoading />}
-                       title="Loading Page" />;
-    }
-
-    if (error)
-      return (
-        <NotFound icon={<IconError />}
-                  title="Opps, something went wrong" />
-      );
-
-    if (!data || data.users.length === 0) {
-      return <NotFound icon={<IconQuestion />}
-                       title="Not Found" />;
-    }
-
-    return (
-      <table className="min-w-full table-auto border-collapse border-none">
-        <thead>
-        {table.getHeaderGroups().map((headerGroup) => (
-          <tr key={headerGroup.id}>
-            {headerGroup.headers.map((header) => (
-              <th
-                key={header.id}
-                className="sticky top-0 first:z-10 first:left-0 p-[7px] bg-[#F7F7F8] border border-solid border-[#EAEDF0] leading-[12px] tracking-[0.2px] uppercase text-left font-semibold text-[10px] text-[#5F6E7C] border-t-0 first:border-l-0 last:border-r-0"
-              >
-                {flexRender(
-                  header.column.columnDef.header,
-                  header.getContext()
-                )}
-              </th>
-            ))}
-          </tr>
-        ))}
-        </thead>
-        <tbody>
-        {table.getRowModel().rows.map((row) => (
-          <tr key={row.id}>
-            {row.getVisibleCells().map((cell) => (
-              <td
-                key={cell.id}
-                className="first:min-w-[180px] first:sticky first:left-0 py-[12px] px-[8px] bg-[#FFFFFF] border border-solid border-[#EAEDF0] text-left font-normal text-[13px] text-[#202932] whitespace-nowrap first:border-l-0 last:border-r-0"
-              >
-                {flexRender(cell.column.columnDef.cell, cell.getContext())}
-              </td>
-            ))}
-          </tr>
-        ))}
-        </tbody>
-      </table>
-    );
   };
 
   const handlePageChange = (page: number) => {
@@ -231,7 +64,10 @@ const Users: React.FC = () => {
           onToggleColumn={toggleColumnVisibility}
         />
         <div className="h-[550px] overflow-x-auto relative border border-solid border-[#EAEDF0] rounded-tl-[12px] rounded-tr-[12px] bg-[#f7faf9]">
-          {renderUsersTableContent()}
+          <UsersTable data={data}
+                      error={error}
+                      isLoading={isLoading}
+                      visibleColumns={visibleColumns} />
         </div>
         <Pagination
           totalItems={data?.total}
---
+++ src/pages/Users/UsersTable.tsx
@@ -0,0 +1,183 @@
+import React, { useMemo } from 'react';
+import moment from 'moment/moment';
+import { flexRender, getCoreRowModel, Row, useReactTable } from '@tanstack/react-table';
+import { CustomColumnDef } from './Users.tsx';
+import { User, UsersData } from '../../app/services/users.ts';
+import NotFound from '../../components/UIComponents/EmptyState/EmptyState.tsx';
+import IconLoading from '../../components/Icons/IconLoading.tsx';
+import IconError from '../../components/Icons/IconError.tsx';
+import IconQuestion from '../../components/Icons/IconQuestion.tsx';
+import IconMale from '../../components/Icons/IconMale.tsx';
+import IconFemale from '../../components/Icons/IconFemale.tsx';
+
+interface UsersTableProps {
+  data: UsersData | undefined;
+  error: unknown;
+  isLoading: boolean;
+  visibleColumns: { [key: string]: boolean };
+}
+
+const UsersTable: React.FC<UsersTableProps> = ({data, error, isLoading, visibleColumns}) =>{
+  const columns: CustomColumnDef<User>[] = useMemo(
+    () =>
+      [
+        {
+          header: 'Full Name',
+          cell: ({row}: { row: Row<User> }) => {
+            const {firstName, lastName, image} = row.original;
+
+            return (
+              <div className="flex flex-row items-center">
+                <img
+                  src={image}
+                  className="w-[32px] h-[32px] mr-[4px] border border-solid border-[rgba(0, 0, 0, 0.34)] rounded-full"
+                  alt=""
+                />
+                <span className="font-medium">
+                  {firstName} {lastName}
+                </span>
+              </div>
+            );
+          },
+        },
+        {
+          header: 'Birthday',
+          cell: ({row}: { row: Row<User> }) => {
+            const {birthDate, age} = row.original;
+
+            return (
+              <>
+                {moment(birthDate).format('DD.MM.YYYY')} ({age} years old)
+              </>
+            );
+          },
+        },
+        {
+          header: 'Gender',
+          cell: ({row}: { row: Row<User> }) => {
+            const {gender} = row.original;
+
+            return (
+              <>
+                {gender === 'male' ? (
+                  <div className="flex flex-row items-center">
+                    <IconMale /> Male
+                  </div>
+                ) : (
+                  <div className="flex flex-row items-center">
+                    <IconFemale /> Female
+                  </div>
+                )}
+              </>
+            );
+          },
+        },
+        {
+          header: 'Email',
+          accessorKey: 'email',
+        },
+        {
+          header: 'Phone',
+          accessorKey: 'phone',
+        },
+        {
+          header: 'Username',
+          accessorKey: 'username',
+        },
+        {
+          header: 'General info',
+          cell: ({row}: { row: Row<User> }) => {
+            const {bloodGroup, height, weight, hair} = row.original;
+
+            return (
+              <>
+                Bloodgroup: "{bloodGroup}"; Height: {Math.floor(height)};
+                Weight: {Math.floor(weight)}; Hair color: {hair.color}
+              </>
+            );
+          },
+        },
+        {
+          header: 'Domain',
+          accessorKey: 'userAgent',
+        },
+        {
+          header: 'IP',
+          accessorKey: 'ip',
+        },
+        {
+          header: 'Mac IP',
+          accessorKey: 'macAddress',
+        },
+      ].filter((column) => visibleColumns[column.header]),
+    [visibleColumns]
+  );
+
+  const table = useReactTable({
+    data: data?.users ?? [],
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
+
+  const renderUsersTableContent = () => {
+    if (isLoading) {
+      return <NotFound icon={<IconLoading />}
+                       title="Loading Page" />;
+    }
+
+    if (error)
+      return (
+        <NotFound icon={<IconError />}
+                  title="Opps, something went wrong" />
+      );
+
+    if (!data || data.users.length === 0) {
+      return <NotFound icon={<IconQuestion />}
+                       title="Not Found" />;
+    }
+
+    return (
+      <table className="min-w-full table-auto border-collapse border-none">
+        <thead>
+        {table.getHeaderGroups().map((headerGroup) => (
+          <tr key={headerGroup.id}>
+            {headerGroup.headers.map((header) => (
+              <th
+                key={header.id}
+                className="sticky top-0 first:z-10 first:left-0 p-[7px] bg-[#F7F7F8] border border-solid border-[#EAEDF0] leading-[12px] tracking-[0.2px] uppercase text-left font-semibold text-[10px] text-[#5F6E7C] border-t-0 first:border-l-0 last:border-r-0"
+              >
+                {flexRender(
+                  header.column.columnDef.header,
+                  header.getContext()
+                )}
+              </th>
+            ))}
+          </tr>
+        ))}
+        </thead>
+        <tbody>
+        {table.getRowModel().rows.map((row) => (
+          <tr key={row.id}>
+            {row.getVisibleCells().map((cell) => (
+              <td
+                key={cell.id}
+                className="first:min-w-[180px] first:sticky first:left-0 py-[12px] px-[8px] bg-[#FFFFFF] border border-solid border-[#EAEDF0] text-left font-normal text-[13px] text-[#202932] whitespace-nowrap first:border-l-0 last:border-r-0"
+              >
+                {flexRender(cell.column.columnDef.cell, cell.getContext())}
+              </td>
+            ))}
+          </tr>
+        ))}
+        </tbody>
+      </table>
+    );
+  };
+
+  return (
+    <>
+      {renderUsersTableContent()}
+    </>
+  );
+}
+
+export default UsersTable;
