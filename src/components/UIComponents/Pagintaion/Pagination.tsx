9b90d1bc41afc9629a2cebaffe0e767d67dd9991
Gleb Bigma <glebbigma@gmail.com>
2024-11-19 17:29:43 +0200

    Added UI for pagination component.
    Took 22 minutes

--- src/components/UIComponents/Pagintaion/Pagination.tsx
+++ src/components/UIComponents/Pagintaion/Pagination.tsx
@@ -2,6 +2,8 @@ import React, {useState} from 'react';
 import * as Select from '@radix-ui/react-select';
 import SelectItemsPerPageItem from './SelectItemsPerPage/SelectItemsPerPageItem.tsx';
 import SelectItemsPerPage from './SelectItemsPerPage/SelectItemsPerPage.tsx';
+import IconArrowLeft from '../../Icons/IconArrowLeft.tsx';
+import IconArrowRight from '../../Icons/IconArrowRight.tsx';
 
 interface PaginationProps {
   totalItems?: number;
@@ -49,10 +51,10 @@ const Pagination: React.FC<PaginationProps> = ({
   };
 
   const getButtonClass = (isDisabled: boolean) =>
-    isDisabled ? 'text-gray-400' : 'text-blue-500';
+    isDisabled ? 'opacity-50' : '';
 
   return (
-    <div className="flex items-center justify-between p-4 border-t border-gray-200">
+    <div className="flex items-center justify-between p-[7px] rounded-bl-[12px] rounded-br-[12px] border border-t-0 border-[rgba(234, 237, 240, 1)]">
       <SelectItemsPerPage name="Items per page"
                           value={itemsPerPage}
                           onValueChange={setItemsPerPage}
@@ -64,37 +66,36 @@ const Pagination: React.FC<PaginationProps> = ({
         </Select.Group>
       </SelectItemsPerPage>
 
-      <div className="text-sm text-gray-600">
-        {`${(currentPage - 1) * itemsPerPage + 1} - ${Math.min(
-          currentPage * itemsPerPage,
-          totalItems
-        )} OF ${totalItems}`}
-      </div>
-
-      <div className="flex items-center space-x-2">
-        <button
-          className={`p-1 ${getButtonClass(currentPage === 1)}`}
-          onClick={() => handlePageChange(currentPage - 1)}
-          disabled={currentPage === 1}
-        >
-          <span className="material-icons">chevron_left</span>
-        </button>
-
-        <input
-          type="text"
-          value={inputPage}
-          onChange={handleInputChange}
-          onKeyDown={handleKeyPress}
-          className="w-12 text-center border border-gray-300 rounded"
-        />
-
-        <button
-          className={`p-1 ${getButtonClass(currentPage === totalPages)}`}
-          onClick={() => handlePageChange(currentPage + 1)}
-          disabled={currentPage === totalPages}
-        >
-          <span className="material-icons">chevron_right</span>
-        </button>
+      <div className="flex items-center">
+        <div className="mr-[12px]  font-semibold uppercase text-[10px] tracking-[0.2px] text-[#5F6E7C]">
+          {`${(currentPage - 1) * itemsPerPage + 1} - ${Math.min(
+            currentPage * itemsPerPage,
+            totalItems
+          )} OF ${totalItems}`}
+        </div>
+        <div className="flex items-center space-x-2">
+          <button
+            className={`p-1 ${getButtonClass(currentPage === 1)}`}
+            onClick={() => handlePageChange(currentPage - 1)}
+            disabled={currentPage === 1}
+          >
+            <IconArrowLeft />
+          </button>
+          <input
+            type="text"
+            value={inputPage}
+            onChange={handleInputChange}
+            onKeyDown={handleKeyPress}
+            className="w-[64px] h-[36px] bg-[#f7faf9] border rounded-[8px] border-[rgba(234, 237, 240, 1)] text-center leading-[20px] text-[13px] placeholder:font-normal placeholder:text-[13px] placeholder:text-[#5F6E7C] active:border-[rgba(0, 92, 178, 1)] focus:text-[13px] focus:text-[rgba(32, 41, 50, 1)]"
+          />
+          <button
+            className={`p-1 ${getButtonClass(currentPage === totalPages)}`}
+            onClick={() => handlePageChange(currentPage + 1)}
+            disabled={currentPage === totalPages}
+          >
+            <IconArrowRight />
+          </button>
+        </div>
       </div>
     </div>
   );
--- src/features/Users/UsersTable.tsx
+++ src/features/Users/UsersTable.tsx
@@ -221,7 +221,7 @@ const UsersTable: React.FC = () => {
           visibleColumns={visibleColumns}
           onToggleColumn={toggleColumnVisibility}
         />
-        <div className='h-[550px] overflow-x-auto relative border border-solid border-[#EAEDF0] rounded-[12px] bg-[#f7faf9]'>
+        <div className='h-[550px] overflow-x-auto relative border border-solid border-[#EAEDF0] rounded-tl-[12px] rounded-tr-[12px] bg-[#f7faf9]'>
           {renderUsersTableContent()}
         </div>
         <Pagination
